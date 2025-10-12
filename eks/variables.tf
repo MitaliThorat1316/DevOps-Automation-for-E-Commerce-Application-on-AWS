452eaf43dffb6443bb4132cb1d76dc9e2652b918
MitaliThorat1316 <120464667+MitaliThorat1316@users.noreply.github.com>
2025-10-12 13:53:24 +0100

    add eks

---
+++ eks/variables.tf
@@ -0,0 +1,67 @@
+variable "region" {
+  description = "AWS region"
+  type        = string
+  default     = "eu-west-2"
+}
+
+variable "vpc_cidr" {
+  description = "CIDR block for VPC"
+  type        = string
+  default     = "10.0.0.0/16"
+}
+
+variable "availability_zones" {
+  description = "Availability zones"
+  type        = list(string)
+  default     = ["eu-west-2a", "eu-west-2b", "eu-west-2c"]
+}
+
+variable "private_subnet_cidrs" {
+  description = "CIDR blocks for private subnets"
+  type        = list(string)
+  default     = ["10.0.100.0/24", "10.0.101.0/24", "10.0.102.0/24"]
+}
+
+variable "public_subnet_cidrs" {
+  description = "CIDR blocks for public subnets"
+  type        = list(string)
+  default     = ["10.0.110.0/24", "10.0.111.0/24", "10.0.112.0/24"]
+}
+
+variable "cluster_name" {
+  description = "Name of the EKS cluster"
+  type        = string
+  default     = "my-eks-cluster"
+}
+
+variable "cluster_version" {
+  description = "Kubernetes version"
+  type        = string
+  default     = "1.30"
+}
+
+variable "node_groups" {
+  description = "EKS node group configuration"
+  type = map(object({
+    instance_types = list(string)
+    capacity_type  = string
+    scaling_config = object({
+      desired_size = number
+      max_size     = number
+      min_size     = number
+    })
+  }))
+  default = {
+    general = {
+      instance_types = ["t3.large"] #change made by me
+      #instance_types = ["t3.medium"]  #original
+      #instance_types = ["t3.micro"] #change made by me
+      capacity_type  = "ON_DEMAND"
+      scaling_config = {
+        desired_size = 1
+        max_size     = 1
+        min_size     = 1
+      }
+    }
+  }
+}
